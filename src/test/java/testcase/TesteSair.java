0e85f53adb4e05630a85703e71850f62c4ed41ea
Delio <Delio@192.168.0.6>
2016-04-07 21:49:58 -0300

    Testes Maven CI Final 1

--- src/test/java/testcase/TesteSair.java
+++ src/test/java/testcase/TesteSair.java
@@ -16,12 +16,7 @@ import common.TestCaseCommon;
 public class TesteSair {
 	
 	private StringBuffer verificationErrors = new StringBuffer();
-<<<<<<< HEAD
-	
-=======
 
-
->>>>>>> branch 'master' of https://github.com/deliodamin/TrabFinalTestesMaven.git
 	@Before
 	public void setUp() throws Exception {
 			TestCaseCommon.getDriver().get(TestCaseCommon.URL_BASE + "/");
@@ -36,7 +31,7 @@ public class TesteSair {
 		if (!"".equals(verificationErrorString)) {
 			fail(verificationErrorString);
 		}
-		TestCaseCommon.getDriver().quit();
+		TestCaseCommon.getDriver().close();
 	}
 	
 	 @Test
@@ -48,22 +43,7 @@ public class TesteSair {
 			System.out.println(RetornoDeObjetos.get(0));
 		    assertEquals("Sair", RetornoDeObjetos.get(0));
 		    SairApp.FecharBrowser();
-<<<<<<< HEAD
-=======
-		
->>>>>>> branch 'master' of https://github.com/deliodamin/TrabFinalTestesMaven.git
 	  }
-	 
-	 @After
-		public void tearDown() throws Exception {
-
-
-			String verificationErrorString = verificationErrors.toString();
-			if (!"".equals(verificationErrorString)) {
-				fail(verificationErrorString);
-			}
-			TestCaseCommon.getDriver().close();
-		}
-	 
+	
 	 
 }
